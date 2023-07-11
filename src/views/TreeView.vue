67aafcb3f6afbf5d65c8fdcc7058bfb6e6ac4b30
pitang1965 <pitang1965@gmail.com>
2023-07-11 15:30:29 +0900

    💄 [フォルダを追加]等のボタンを追加。disabled関連のみ実装。

--- src/views/TreeView.vue
+++ src/views/TreeView.vue
@@ -2,9 +2,46 @@
   <div class="container mx-auto px-2">
     <h1 class="font-bold">ツリー</h1>
     <div class="flex mt-4">
-      <h2>label: {{ currentLabel ? currentLabel : '未選択' }}</h2>
+      <h2>label: {{ currentLabel }}</h2>
       <h2 class="ml-2">フォルダ: {{ isFolder ? 'Yes' : 'No' }}</h2>
       <h2 class="ml-2">id: {{ currentId ? currentId : '未選択' }}</h2>
+    </div>
+
+    <h2>addFolderDisabled: {{ addFolderDisabled }}</h2>
+
+    <div class="flex gap-2">
+      <button
+        :disabled="addFolderDisabled"
+        @click="addFolder"
+        class="text-white px-2 my-2 rounded-full"
+        :class="{'bg-purple-500': !addFolderDisabled, 'bg-gray-300': addFolderDisabled, 'hover:bg-purple-700': !addFolderDisabled}"
+      >
+        フォルダを追加
+      </button>
+      <button
+        :disabled="addDocumentDisabled"
+        @click="addDocument"
+        class="text-white px-2 my-2 rounded-full"
+        :class="{'bg-purple-500': !addDocumentDisabled, 'bg-gray-300': addDocumentDisabled, 'hover:bg-purple-700': !addDocumentDisabled}"
+      >
+        文書を追加
+      </button>
+      <button
+      :disabled="deleteDisabled"
+        @click="deleteItem"
+        class="text-white px-2 my-2 rounded-full"
+        :class="{'bg-purple-500': !deleteDisabled, 'bg-gray-300': deleteDisabled, 'hover:bg-purple-700': !deleteDisabled}"
+      >
+        削除
+      </button>
+      <button
+      :disabled="renameDisabled"
+        @click="renameItem"
+        class="text-white px-2 my-2 rounded-full"
+        :class="{'bg-purple-500': !renameDisabled, 'bg-gray-300': renameDisabled, 'hover:bg-purple-700': !renameDisabled}"
+      >
+        改名
+      </button>
     </div>
 
     <CustomTree :data="data" @selected="selectedHandler" />
@@ -12,7 +49,7 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { computed, ref } from 'vue'
 import CustomTree from '@/components/CustomTree.vue'
 import createData from '@/data/createData'
 
@@ -28,6 +65,42 @@ const selectedHandler = ({ id, label, isFolder: isFolderValue }) => {
   isFolder.value = isFolderValue
 }
 
+const addFolder = () => {
+  alert('フォルダ追加')
+}
+
+const addDocument = () => {
+  alert('文書追加')
+}
+
+const deleteItem = () => {
+  alert('削除')
+}
+
+const renameItem = () => {
+  alert('改名')
+}
+
+// [フォルダを追加]が使用不可
+const addFolderDisabled = computed(() => {
+  if (isFolder.value) return false
+  return !!currentId.value
+})
+
+// [文書を追加]が使用不可
+const addDocumentDisabled = computed(() => !isFolder.value) 
+
+// [削除]が使用不可
+const deleteDisabled = computed(() => {
+  if (isFolder.value && currentLabel.value === '未分類') return true
+  return !!!currentId.value
+  })
+
+// [改名]が使用不可
+const renameDisabled = computed(() => {
+  if (isFolder.value && currentLabel.value === '未分類') return true
+  return !!!currentId.value
+  })
 </script>
 
 <style></style>
