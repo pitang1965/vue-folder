5065effe3903c39107dbe8c70b1aaab547924682
pitang1965 <pitang1965@gmail.com>
2023-08-01 18:15:21 +0900

    🐛 ライブラリ変更でクリックされたときの処理が失われていた

--- src/components/CustomTree.vue
+++ src/components/CustomTree.vue
@@ -4,10 +4,11 @@
     :data="data"
     :default-expanded-keys="defaultExpandedKeys"
     :selectable="false"
+    :node-props="nodeProps"
   />
 </template>
 
-<script setup lang="ts" >
+<script setup lang="ts">
 import { onUpdated, ref } from 'vue'
 
 const props = defineProps({
@@ -19,17 +20,21 @@ const props = defineProps({
 
 const emit = defineEmits(['selected'])
 
-const clickHandler = (data: TreeNodeData, node: TreeNode) => {
-  const isFolder = !node.isLeaf
-  emit('selected', { id: data.id, label: data.label, isFolder })
-}
-
 onUpdated(() => {
   console.log('props.dataが変更！')
 })
 
 const defaultExpandedKeys = ref(['40', '41'])
 
+const nodeProps = ({ option }: { option: TreeOption }) => {
+  // クリックされたときの処理
+  const onClick = () => {
+    const isFolder = typeof(option.children) !== 'undefined'
+    emit('selected', { id: option.key, label: option.label, isFolder })
+    console.log(option)
+  }
+  return { onClick }
+}
 </script>
 
 <style></style>
