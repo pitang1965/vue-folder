28bc2b35fec99eeb1f04fca8f8a6fe4aeca8fe0a
pitang1965 <pitang1965@gmail.com>
2023-08-07 11:37:15 +0900

    ✨ ツリーの展開状態をlocalStorageに保存

--- src/components/CustomTree.vue
+++ src/components/CustomTree.vue
@@ -2,15 +2,16 @@
   <n-tree
     block-line
     :data="data"
-    :default-expanded-keys="defaultExpandedKeys"
+    :default-expanded-keys="expandedKeys"
     :selectable="false"
     :node-props="nodeProps"
     :render-switcher-icon="renderSwitcherIconWithExpaned"
+    :on-update:expanded-keys="handleExpandedKeysUpdate"
   />
 </template>
 
 <script setup lang="ts">
-import { h, ref } from 'vue';
+import { h, onMounted, ref, watch } from 'vue';
 import { NIcon, TreeOption } from 'naive-ui';
 import { FolderOpenOutline, FolderOutline } from '@vicons/ionicons5';
 
@@ -24,7 +25,7 @@ const props = defineProps({
 const emit = defineEmits(['selected']);
 
 // 最初に展開するノード
-const defaultExpandedKeys = ref(['30', '31']);
+const expandedKeys = ref([]);
 
 const nodeProps = ({ option }: { option: TreeOption }) => {
   // クリックされたときの処理
@@ -40,6 +41,41 @@ const renderSwitcherIconWithExpaned = ({ expanded }: { expanded: boolean }) =>
   h(NIcon, null, {
     default: () => h(expanded ? FolderOpenOutline : FolderOutline)
   });
+
+const handleExpandedKeysUpdate = (keys, options, { node, action }) => {
+  expandedKeys.value = keys;
+
+  console.log('Updated Expanded Keys:', keys);
+  console.log('Options:', options);
+  console.log('Node:', node);
+  console.log('Action:', action);
+
+  // 必要に応じて追加の処理を行う
+  if (action === 'expand') {
+    console.log(`${node.label} has been expanded!`);
+  } else if (action === 'collapse') {
+    console.log(`${node.label} has been collapsed!`);
+  }
+};
+
+const loadFromLocalStorage = () => {
+  const item = localStorage.getItem('expandedKeys');
+  if (item) {
+    expandedKeys.value = JSON.parse(item);
+  }
+};
+
+const saveToLocalStorage = () => {
+  const keys = localStorage.setItem('expandedKeys', JSON.stringify(expandedKeys.value));
+};
+
+onMounted(() => {
+  loadFromLocalStorage();
+});
+
+watch (expandedKeys, async(newKeys) => {
+  saveToLocalStorage();
+});
 
 </script>
 
